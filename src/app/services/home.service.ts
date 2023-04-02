944eb1462121fa882facbb2abf71ba1cacf1d824
danielgo <danideveloperweb@gmail.com>
2023-04-02 11:37:04 +0200

    library

--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -2,7 +2,6 @@
 import { Injectable } from '@angular/core';
 import { EsPlayer } from './model/esPlayers';
 import { IEsPlayer } from './model/esPlayers.interface';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +9,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class HomeService {
 
   esPlayer: IEsPlayer[] = EsPlayer;
-  // video!: IEsPlayer;
-  // sanitazedUrl: SafeResourceUrl[] = [];
-  // constructor(private _sanitazer: DomSanitizer) {
-  // }
+
   getEsPlayer(): IEsPlayer[] {
     return this.esPlayer;
   }
@@ -22,11 +18,4 @@ export class HomeService {
     return this.esPlayer[id - 1];
   }
 
-
-  // getvid(id: any) {
-  //   const player = this.getEsPlayerDetail(id);
-  //   player.vid.forEach(element => {
-  //     this.sanitazedUrl.push(this._sanitazer.bypassSecurityTrustResourceUrl(element))
-  //   });return this.sanitazedUrl;
-  // }
 }
